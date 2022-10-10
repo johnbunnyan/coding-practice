801ccfd7deed427dd5f75b8a9659a1e2c5ac5a0d
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-10 12:30:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("입력")
+
+# n,m = map(int, input().split())
+# print("네가 입력한 숫자 :",n, m)
+
+print(n+m)
+print(n-m)
+print(n*m)
+print(round(n/m))
+print(n%m)
---
+++ template.py
@@ -0,0 +1,9 @@
+print("입력")
+
+n = int(input())
+print("네가 입력한 숫자 :",n)
+
+
+result = 0
+
+print("마지막 숫자:",result)
