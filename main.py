f8e5c438e4bf34b9e98d7259c2acb2bad6b26a64
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-13 06:33:58 +0000

    3003번 문제

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-print("입력")
+nums = list(map(int, input().split()))
 
-# n,m = map(int, input().split())
-# print("네가 입력한 숫자 :",n, m)
 
-print(n+m)
-print(n-m)
-print(n*m)
-print(round(n/m))
-print(n%m)
+for i in range(len(nums)):
+  if i == 0 or i == 1 : 
+    nums[i] = 1 - nums[i]
+
+  elif i == 2 or i == 3 or i == 4 : nums[i] = 2 -nums[i]
+      
+  elif i == 5 :
+      nums[i] = 8 -nums[i]
+ 
+stringfy = ' '.join(map(str,nums))
+print(stringfy)
