0aa6820c2e82b4d6952ac2ecfbe48361dde3bc3f
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-13 06:42:05 +0000

    10430번: 나머지

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-nums = list(map(int, input().split()))
+A, B, C = list(map(int, input().split()))
+
+# (A+B)%C
+# ((A%C) + (B%C))%C
+# (A×B)%C
+# ((A%C) × (B%C))%C 
+
+first = (A+B)%C
+second = ((A%C) + (B%C))%C
+third = (A*B)%C
+forth = ((A%C) * (B%C))%C 
 
 
-for i in range(len(nums)):
-  if i == 0 or i == 1 : 
-    nums[i] = 1 - nums[i]
-
-  elif i == 2 or i == 3 or i == 4 : nums[i] = 2 -nums[i]
-      
-  elif i == 5 :
-      nums[i] = 8 -nums[i]
- 
-stringfy = ' '.join(map(str,nums))
-print(stringfy)
+print(first)
+print(second)
+print(third)
+print(forth)
