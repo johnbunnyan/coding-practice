39206931285ef1bda852f0f95b9c9d59928542b3
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-14 06:42:44 +0000

    1330:두 수 비교하기

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-first = int(input())
-second = input()
-second_last = int(second[-1])
-second_mid = int(second[-2])
-second_first = int(second[-3])
+n,m = map(int,input().split())
 
-# print(second_last)
-# print(second_mid)
-# print(second_first)
-
-third=first * second_last
-forth=first * second_mid * 10
-fifth=first * second_first * 100
-sixth=third+forth+fifth
-
-print(third)
-print(forth // 10)
-print(fifth // 100)
-print(sixth)
-
-
+if n<m: 
+    print('<')
+elif n>m:
+    print('>')
+elif n==m:
+    print('=')
