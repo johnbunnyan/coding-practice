8f21f6530baf56f6c170e2207177555b117c6a3a
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-14 07:14:36 +0000

    9428:시험성적

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-n,m = map(int,input().split())
+n = int(input())
 
-if n<m: 
-    print('<')
-elif n>m:
-    print('>')
-elif n==m:
-    print('=')
+if n<=100 and n>=90: 
+    print('A')
+elif n<=89 and n>=80: 
+    print('B')
+elif n<=79 and n>=70: 
+    print('C')
+elif n<=69 and n>=60: 
+    print('D')
+else: print('F')
