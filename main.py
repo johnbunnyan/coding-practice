6a439b9ca6c0cd9132c08346f14e2cb6cb9c57b2
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-15 13:29:10 +0000

    2739:구구단 (문자열 출력 포멧)

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-first, second, third = map(int,input().split())
+n = int(input())
 
-reward = 0
-
-if (first == second) and (second == third) and (first == third):
-  reward = 10000+first*1000
-  
-elif (first == second != third) or (second == third != first) or (first == third != second):
-  if first == second:
-    reward = 1000+first*100
-  elif second == third:
-    reward = 1000+second*100
-  elif first == third:
-    reward = 1000+third*100
-
-elif (first != second) and (second != third) and (first != third):
-  largest = max(first, second, third)
-  reward = largest * 100
-
-print(reward)
+for i in range(1,10):
+  multiply = n * i
+  print("{} * {} = {}".format(n, i, multiply))
