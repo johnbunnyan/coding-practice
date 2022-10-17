1eec97d82af420e7229c3952f3678f3a9d8c02a3
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-17 07:00:25 +0000

    15552:빠른 A+B

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-n = int(input())
+import sys
 
-for i in range(1,10):
-  multiply = n * i
-  print("{} * {} = {}".format(n, i, multiply))
+total = int(sys.stdin.readline().rstrip())
+
+for i in range(total):
+  # 한개짜리는 잘 받을 수 있다.
+  # a = int(sys.stdin.readline().rstrip())
+  # print("ww",type(a))
+
+  # 한 줄에 두개짜리 받기(한번에 1,2 수행하지 않기!)
+ # 1. 일단 입력받기
+   a = sys.stdin.readline().rstrip()
+  # 2. 입력받은 문자열 나누고 int 매핑
+  b, c = map(int, a.split())
+   print(b+c)
+
+
+
+
