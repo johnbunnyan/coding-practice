98237392e163f13ed224062eec42be1f2e9863ae
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-10-28 04:30:46 +0000

    10951:A+B-4

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-n = int(input())
 
-for i in range(n):
-  for j in range(n-1,-1,-1):
-    #end키워드를 쓰면 print를 해도 다음줄로 넘어가지 않는다
-  
-    if i>=j: print("*", end='')
-    else: print(' ',end='')
-  if(i != n): print('')
+while True:
+  try:
+    num1, num2 = map(int, input().split())
+    print(num1 + num2)
+  except EOFError:
+    break
 
 
-# 문제는 다 풀 수 있다
-# 못 푸는 건 게을러서다
-# 패턴을 하나하나 적고
-# 규칙을 파악하면 다 풀 수 있는 것
-# 그래도 안되면 검색하면 다 됨
-# ->
-# https://codingcoding.tistory.com/1103#h_11
+# 검색으로 해결
+# 파이썬 입력이 끝날때까지 받는 경우(End Of File, EOFerror)
+# https://velog.io/@kimjhq1/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%A0%A5%EC%9D%B4-%EB%81%9D%EB%82%A0%EB%95%8C%EA%B9%8C%EC%A7%80-%EB%B0%9B%EB%8A%94-%EA%B2%BD%EC%9A%B0End-Of-File-EOFerror
