0f555abb7c3b360031305147263a610ade274338
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-11-02 12:05:29 +0000

    5597:과제 안 내신 분...?(주어진 테스트 케이스 외 추가로 예외 케이스를 생각해보기)

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# loop = int(input())
+# 3
+# 2
+# 4
+# 5
+# 7
+# 8
+# 9
+# 6
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+# 21
+# 22
+# 23
+# 24
+# 25
+# 26
+# 27
+# 28
+# 29
 
-# arr =  list(map(int,input().split()))
 
-max = 0
-index=0
 
-for i in range(9):
+
+arr = []
+
+for i in range(28):
   num = int(input())
-  if num > max:
-    max = num
-    index = i+1
+  arr.append(num)
 
-print(max)
-print(index)
+arr.sort()
+# print(arr)
+result = []
+
+for i in range(1,31):
+  if i not in arr:
+    print(i)
+  
+
+# <리스트 정렬 하기 - sort>
+# https://blockdmask.tistory.com/564
     
+# <리스트 안에 어떤 요소 있는지 체크 - not in>  https://pydole.tistory.com/entry/Python-%ED%8F%AC%ED%95%A8Containment-%EC%97%B0%EC%82%B0%EC%9E%90-in-not-in
