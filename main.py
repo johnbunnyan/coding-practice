e8ba071c272bd39c7c8ef82d4c25d4a2aacb0faf
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-11-03 11:08:44 +0000

    8958:OX퀴즈(print로 변수들 추적하기)

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 total = int(input())
-arr = list(map(int, input().split()))
 
-arr.sort()
+result = []
 
-max = arr[-1]
+for i in range(total):
+  text = list(input())
+  plus = 0
+  cnt=0
+  score=0
 
-newArr = []
+  for j in range(len(text)):
+    # print("score",score)
+    if text[j] == 'O':
+      plus += 1
+      cnt += plus
+      # print("Oplus",plus)
+      # print("Ocnt",cnt)
+    if text[j] == 'X':
+      score += cnt
+      cnt = 0
+      plus = 0
+      # print("Xplus",plus)
+      # print("Xcnt",cnt)
+    if j == len(text)-1:
+      score += cnt
+      cnt = 0
+      result.append(score)
 
-for i in range(len(arr)):
-  newNum = arr[i]/max*100
-  newArr.append(newNum)
 
-avg = sum(newArr)/len(newArr)
-print(avg)
-
+for i in range(len(result)):
+  print(result[i])
+      
+# 변수가 많으면 print로 변수추적하기를 주저하지 말자!
