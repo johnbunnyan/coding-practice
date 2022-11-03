97fa30ff360e935f510c08e5735c2639479e0713
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2022-11-03 12:01:51 +0000

    15596:정수 N개의 합

--- main.py
+++ main.py
@@ -3,30 +3,11 @@
 # https://seopark.tistory.com/24
 # 2. 반올림 메서드
 
+def solve(a):
+    ans = 0
+    for i in range(len(a)):
+        ans += a[i]
+ 
+    return ans
 
-total = int(input())
-
-result = []
-
-for i in range(total):
-  spread = list(map(int,input().split(' ')))
-  amount = spread[0]
-  students = spread[1:]
-  # print(amount)
-  # print(students)
-  avg = sum(students)/amount
-  # print("avg",avg)
-
-  # 평균넘는 학생 비율 = 평균넘는 수/전체수
-  overed=0
-  for j in range(amount):
-    if students[j] > avg:
-      overed += 1
-
-    if j == amount-1:
-      rate = round(overed/amount*100,3)
-      formatting = format(rate,".3f")
-      print(str(formatting)+'%')
-    
-    
-  
+print(solve([1,2,3]))
