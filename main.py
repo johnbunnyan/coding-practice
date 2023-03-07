62302edfbe690aa9ff9068797e8a88a932b8e851
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2023-03-07 07:33:07 +0000

    4949(스택-균형잡힌 세상)

--- main.py
+++ main.py
@@ -1,51 +1,41 @@
-# 예제 보면서 패턴 파악
-# 정답인 예제와 오답인 예제의 차이 도출하기
-# 오답 케이스
-# (()) ())
-# (() ((()) () (
-# (())()))
-# (()
-# (과 )의 짝이 맞아야 한다
-
-# 정답 케이스
-# (()()) ((()))
-# () () () () (()()()) ()
-# (())()
-# ((()))
-
-# 인간 뇌피셜(나의 방식)으로 문제풀기
-
-
-# 코드 로직으로 옮기기
-# (가 나오면 스택으로 쌓는다
-# )가 나오기 시작하면 )만 연달아 나와야한다
-# 그리고 그 개수가 (가 쌓인 개수와 같아야 한다
-# 짝이 맞으면 초기화
-# 초기화했는데 (가 아니라 )가 나오면 탈락
-
 import sys
-length = int(input())
 
 stack = []
 
-for i in range(length):
-  elem = list(sys.stdin.readline().strip())
 
+while True:
+  elem = list(sys.stdin.readline().rstrip())
+  if elem[0] == '.':
+    break
+  
   for unit in elem:
     if unit == '(':
       stack.append('(')
-  
-    elif unit == ')':
+    elif unit == '[':
+      stack.append('[')
+
+    if unit == ')':
       if len(stack) != 0 and stack[-1] == '(':
         stack.pop()
-      elif len(stack) == 0:
+      elif len(stack) == 0 or stack[-1] == '[':
         stack.append(')')
+  
+      
+        
+    elif unit == ']':
+      if len(stack) != 0 and stack[-1] == '[':
+        stack.pop()
+      elif len(stack) == 0 or stack[-1] == '(':
+        stack.append(']')
 
+        
+      
+  # print(stack)
   if len(stack) == 0:
-    print('YES')
+    print('yes')
     stack = []
   else:
-   print('NO')
+   print('no')
    stack = []
 
 
