0b264f0c0e4331aafeaccccd3c32b251548ba353
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2023-03-08 10:25:50 +0000

    24479(그래프와순회-깊우탐1)

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 import sys
+sys.setrecursionlimit(10 ** 6)
 
-stack = []
 
+vertex, edge, current = map(int, input().split())
 
-while True:
-  elem = list(sys.stdin.readline().rstrip())
-  if elem[0] == '.':
-    break
+edges = [[] for _ in range(vertex+1)]
+
+for i in range(edge):
+  edge_from, edge_to = map(int,sys.stdin.readline().split())
+  edges[edge_from].append((edge_to))
+  edges[edge_to].append((edge_from))
   
-  for unit in elem:
-    if unit == '(':
-      stack.append('(')
-    elif unit == '[':
-      stack.append('[')
 
-    if unit == ')':
-      if len(stack) != 0 and stack[-1] == '(':
-        stack.pop()
-      elif len(stack) == 0 or stack[-1] == '[':
-        stack.append(')')
+edges = [sorted(i) for i in edges]
+
+visited = [False] * (vertex+1)
+
+
+result = [0] * (vertex+1)
+count = 0
+
+print(edges)
+def dfs(edges, current, visited):
+  global count
   
-      
-        
-    elif unit == ']':
-      if len(stack) != 0 and stack[-1] == '[':
-        stack.pop()
-      elif len(stack) == 0 or stack[-1] == '(':
-        stack.append(']')
+  # 시작 정점 R을 방문 했다고 표시한다.
+  # list는 0인덱스부터 시작하므로 1뺌
 
-        
-      
-  # print(stack)
-  if len(stack) == 0:
-    print('yes')
-    stack = []
-  else:
-   print('no')
-   stack = []
+  visited[current] = True  
+  count =  count + 1
+  result[current] = count
+  # print("이웃",current)
+  # print("카운트",count)
+  # print(current)
 
+  
+   # edge(current) : 정점 start_vertex의 인접 정점 집합 (정점 번호를 오름차순으로 방문한다)
+  for close_vertex in edges[current]:
 
-   
+    if visited[close_vertex] == False:
+      # print("기준정점",current)
+      # print("방문",visited)
+      # print("여기서는",count)
+      dfs(edges, close_vertex, visited)
 
+          
 
-
+dfs(edges, current, visited)
+for i in range(1,len(result)):
+        print(result[i])
