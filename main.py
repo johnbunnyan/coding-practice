e417986537973b7e1d2750b9be4d39c91980c55d
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2023-04-05 01:50:46 +0000

    10870(재귀)-피보나치 수5

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+# num은 순서
 num = int(input())
 
-def rec(num):
+def fib_rec(num):
   if num < 1:
+    return 0
+  elif num == 1:
     return 1
-  return num * rec(num-1)
+  return fib_rec(num-1) + fib_rec(num-2)
 
-print(rec(num))
+print(fib_rec(num))
   
 
