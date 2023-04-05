fa4b6be8131d2eddac914c135f0e50f64567033b
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2023-04-05 02:30:10 +0000

    25501(재귀)-재귀의 귀재

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# num은 순서
-num = int(input())
+length = int(input())
 
-def fib_rec(num):
-  if num < 1:
-    return 0
-  elif num == 1:
-    return 1
-  return fib_rec(num-1) + fib_rec(num-2)
 
-print(fib_rec(num))
+def recursion(s, l, r, count):
+    count = count+1
+    if l >= r: return 1, count
+    elif s[l] != s[r]: return 0, count
+    else: return recursion(s, l+1, r-1,count)
+
+def isPalindrome(s):
+    count = 0
+    return recursion(s, 0, len(s)-1, count)
   
+for i in range(length):
+  target = input()
+  print(str(isPalindrome(target)[0]) + ' '+ str(isPalindrome(target)[1]))
 
