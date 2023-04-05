d72d1321e3da46d2e1a38912b7104bff80174144
정상규 <73732000+johnbunnyan@users.noreply.github.com>
2023-04-05 02:39:35 +0000

    25501(재귀)-재귀의 귀재 : 주석 추가

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ length = int(input())
 
 def recursion(s, l, r, count):
     count = count+1
+   
+  # AAA
+  # ABABA
+  # 애네들은 가운데 A 하나밖에
+  # 증감한 l,r로 가운데 하나를 판별하는 로직 l >= r
     if l >= r: return 1, count
     elif s[l] != s[r]: return 0, count
     else: return recursion(s, l+1, r-1,count)
